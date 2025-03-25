281a3896ad044cb6f4a59fd9c3447eb439d9ee10
robinixbox <robinixbox@gmail.com>
2025-03-25 19:57:34 +0100

    Initialisation du package agents

---
+++ src/agents/__init__.py
@@ -0,0 +1,13 @@
+from .base_agent import BaseAgent
+from .analyst_agent import AnalystAgent
+from .architect_agent import ArchitectAgent
+from .developer_agent import DeveloperAgent
+from .reviewer_agent import ReviewerAgent
+
+__all__ = [
+    'BaseAgent',
+    'AnalystAgent',
+    'ArchitectAgent',
+    'DeveloperAgent',
+    'ReviewerAgent'
+]
